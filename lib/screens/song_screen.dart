5a29d0363461832d198c80d5ab4dd9048b6d4771
Kshitiz Adhikari <adhikarikshitiz12@gmail.com>
2024-04-15 09:01:33 +0545

    add song page UI

--- lib/screens/song_screen.dart
+++ lib/screens/song_screen.dart
@@ -1,18 +1,178 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:minimal_music_app/components/neu_box.dart';
+import 'package:minimal_music_app/models/playlist_provider.dart';
+import 'package:provider/provider.dart';
 
 class SongScreen extends StatelessWidget {
   const SongScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(title: const Text("S O N G  P A G E"),
-      ),
-      body: Center(
-        child: NeuBox(child: Icon(Icons.abc),),
-      ),
+    return Consumer<PlaylistProvider>(
+      builder: (context, value, child) {
+
+        //get the playlist
+        final playlist = value.playlist;
+
+
+        //get the current song
+        final currentSong = playlist[value.currentSongIndex ?? 0];
+
+        //return the Scaffold UI
+        return Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.background,
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(25),
+              child: Column(
+                children: [
+                  //appbar
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      //back button
+                      IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: Icon(Icons.arrow_back_ios_new_outlined)),
+
+                      //title
+                      Text('S O N G  P A G E'),
+
+                      //menu button
+                      IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  //album artwork
+                  NeuBox(
+                    child: Column(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Image.asset(
+                              currentSong.albumArtImagePath),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            //song and artist name
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    currentSong.songName,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20),
+                                  ),
+                                  Text(currentSong.artistName),
+                                ],
+                              ),
+                            ),
+                            Icon(
+                              Icons.favorite,
+                              color: Colors.red,
+                            ),
+
+                            //heart icon
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  //song duration progress
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            //start time
+                            Text("1:11"),
+
+                            //shuffle icon
+                            Icon(Icons.shuffle),
+
+                            //repeat icon
+                            Icon(Icons.repeat),
+
+                            //end time
+                            Text("3:44"),
+                          ],
+                        ),
+                        SliderTheme(
+                          data: SliderTheme.of(context).copyWith(
+                            thumbShape: const RoundSliderThumbShape(
+                                enabledThumbRadius: 5),
+                          ),
+                          child: Slider(
+                              value: 50,
+                              min: 0,
+                              max: 100,
+                              activeColor: Colors.green,
+                              onChanged: (value) {}),
+                        ),
+                      ],
+                    ),
+                  ),
+                  //playback controls
+                  Row(
+                    children: [
+                      //skip previous
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: NeuBox(
+                            child: Icon(Icons.skip_previous),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 25,
+                      ),
+
+                      //play pause
+                      Expanded(
+                        flex: 2,
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: NeuBox(
+                            child: Icon(Icons.play_arrow),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 25,
+                      ),
+
+                      //skip forward
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: NeuBox(
+                            child: Icon(Icons.skip_next),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
